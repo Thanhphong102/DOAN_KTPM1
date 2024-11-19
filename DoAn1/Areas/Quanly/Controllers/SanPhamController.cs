05deac88af84402a1f255639acd88bcc970c5274
Locwara <lethanhloc2612004@gmail.com>
2024-11-19 15:17:56 +0700

    push 19/11

---
+++ DoAn1/Areas/Quanly/Controllers/SanPhamController.cs
@@ -0,0 +1,70 @@
+﻿using DoAn1.Areas.Quanly.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn1.Areas.Quanly.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: Quanly/SanPham
+        public ActionResult Sanpham()
+        {
+            WebP2HEntities db = new WebP2HEntities();
+            List<SANPHAM> dssanpham = db.SANPHAMs.ToList();
+            return View(dssanpham);
+        }
+        public ActionResult ChitietSP()
+        {
+            WebP2HEntities db = new WebP2HEntities();
+            List<SANPHAM> dssanpham = db.SANPHAMs.ToList();
+            return View(dssanpham);
+        }
+        public ActionResult ThemSP()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ThemSP(SANPHAM model)
+        {
+            WebP2HEntities db = new WebP2HEntities();
+            db.SANPHAMs.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("SanPham", "SanPham");
+        }
+        public ActionResult Danhmuc()
+        {
+            WebP2HEntities db = new WebP2HEntities();
+            List<SANPHAM> dssanpham = db.SANPHAMs.ToList();
+            return View(dssanpham);
+        }
+        public ActionResult ThemDM()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult ThemDM(DANHMUC model)
+        {
+            WebP2HEntities db = new WebP2HEntities();
+            db.DANHMUCs.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Danhmuc","SanPham");
+        }
+        public ActionResult Khachhang()
+        {
+            WebP2HEntities db = new WebP2HEntities();
+            List<KHACHHANG> dssanpham = db.KHACHHANGs.ToList();
+            return View(dssanpham);
+        }
+        public ActionResult Donhang()
+        {
+            WebP2HEntities db = new WebP2HEntities();
+            List<DONHANG> dssanpham = db.DONHANGs.ToList();
+            return View(dssanpham);
+        }
+    }
+}
---
+++ DoAn1/Areas/Quanly/QuanlyAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace DoAn1.Areas.Quanly
+{
+    public class QuanlyAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Quanly";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Quanly_default",
+                "Quanly/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
